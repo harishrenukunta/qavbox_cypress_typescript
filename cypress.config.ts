d20a08ef6c41ba96e6f2e6a207490646554174be
Srujan Chada <srujan145@gmail.com>
2022-12-12 13:02:22 +0000

    Updated config to ignore example tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+	e2e: {
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+		chromeWebSecurity: false,
+		excludeSpecPattern: [
+			'cypress/e2e/1-getting-started/**/*.cy.js',
+			'cypress/e2e/2-advanced-examples/**/*.cy.js',
+		],
+	},
 });
